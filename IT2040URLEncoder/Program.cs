06f5b156d3dccd9408f09fe4bbadcc09adc2551a
Jacob Ross <jross1818@gmail.com>
2018-09-21 19:44:52 -0500

    added full URL Encode functionality

--- IT2040URLEncoder/Program.cs
+++ IT2040URLEncoder/Program.cs
@@ -10,6 +10,118 @@ namespace IT2040URLEncoder
     {
         static void Main(string[] args)
         {
+            String projectName = "";
+            String activityName = "";
+            String newProjectName = "";
+            String newActivityName = "";
+
+
+            Console.WriteLine("Welcome to the URL Encoder!");
+            Console.WriteLine("Project Name?: ");
+            projectName = Console.ReadLine();
+            Console.WriteLine("Activity Name?: ");
+            activityName = Console.ReadLine();
+            newProjectName = URLEncode(projectName);
+            newActivityName = URLEncode(activityName);
+            Console.WriteLine(newProjectName);
+            Console.WriteLine(newActivityName);
+            Console.WriteLine("https://companyserver.com/content/"+newProjectName+"/files/"+newActivityName+"/"+newActivityName+"Report.pdf");
+            Console.ReadLine();
+        }
+        public bool CheckControl(String str){
+            return true;
+        }
+        static String URLEncode(String str){
+           String URL = "";
+
+            foreach(char c in str){
+                switch(c){
+                    case ' ':
+                        URL += "%20";
+                        break;
+                    case '<':
+                        URL += "%3C";
+                        break;
+                    case '>':
+                        URL +="%3E";
+                        break;
+                    case '#':
+                        URL +="%23";
+                        break;
+                    case '%':
+                        URL +="%25";
+                        break;
+                    case '"':
+                        URL +="%22";
+                        break;
+                    case ';':
+                        URL +="%3B";
+                        break;
+                    case '/':
+                        URL += "%2F";
+                        break;
+                    case '?':
+                        URL +="%3F";
+                        break;
+                    case ':':
+                        URL +="%3A";
+                        break;
+                    case '@':
+                        URL +="%40";
+                        break;
+                    case '&':
+                        URL +="%26";
+                        break;
+                    case '=':
+                        URL +="%3D";
+                        break;
+                    case '+':
+                        URL +="%2B";
+                        break;
+                    case '$':
+                        URL +="%24";
+                        break;
+                    case ',':
+                        URL +="%2C";
+                        break;
+                    case '{':
+                        URL +="%7B";
+                        break;
+                    case '}':
+                        URL +="%7D";
+                        break;
+                    case '|':
+                        URL +="%7C";
+                        break;
+                    case '\\':
+                        URL +="%5C";
+                        break;
+                    case '^':
+                        URL +="%5E";
+                        break;
+                    case '[':
+                        URL +="%5B";
+                        break;
+                    case ']':
+                        URL +="%5D";
+                        break;
+                    case '`':
+                        URL +="%60";
+                        break;
+                    default:
+                        URL += c;
+                        break;
+
+
+                }
+            }
+            return URL;
+        
         }
     }
+
+    
+    
+    
 }
+
